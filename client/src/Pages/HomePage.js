ce30b54daa548df313ccbe85fcba0498b0cfc201
rajan <rajan.rana6@gmail.com>
2025-01-10 00:28:52 +0530

    setup home page

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,75 +1,141 @@
-import React, { useState, useEffect } from 'react'
-import Layout from '../Components/Layout/Layout'
-
-import toast from 'react-hot-toast'
-import axios from 'axios'
-import { Checkbox } from 'antd'
-
+import React, { useState, useEffect } from "react";
+import Layout from "./../Components/Layout/Layout";
+import axios from "axios";
+import { Checkbox, Radio } from "antd";
+import { Prices } from "../Components/Prices";
 const HomePage = () => {
-    const [products, setProducts] = useState([])
-    const [Categories, setCategories] = useState([])
-    const [checked, setChecked] = useState([])
+    const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [checked, setChecked] = useState([]);
+    const [radio, setRadio] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
-    //get all categories
-
+    //get all cat
     const getAllCategory = async () => {
         try {
-            const { data } = await axios.get('/api/v1/category/get-category')
-            if (data.success) {
-                setCategories(data.category)
+            const { data } = await axios.get("/api/v1/category/get-category");
+            if (data?.success) {
+                setCategories(data?.category);
             }
-
         } catch (error) {
             console.log(error);
-            toast.error('Error loading categories')
         }
-    }
-    useEffect(() => {
-        getAllCategory()
-    }, [])
+    };
 
-    //get all products
+    useEffect(() => {
+        getAllCategory();
+        getTotal();
+    }, []);
+    //get products
     const getAllProducts = async () => {
         try {
-            const { data } = await axios.get('/api/v1/product/get-product')
-            setProducts(data.products)
+            setLoading(true);
+            const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+            setLoading(false);
+            setProducts(data.products);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+    };
+
+    //getTOtal COunt
+    const getTotal = async () => {
+        try {
+            const { data } = await axios.get("/api/v1/product/product-count");
+            setTotal(data?.total);
         } catch (error) {
             console.log(error);
-            toast.error('Error while fetching products')
         }
-    }
+    };
 
-    //filter by category
-
-    const handleFilter = (value, id) => {
-        let all = [...checked]
-        if (value) {
-            all.push(id)
-        } else {
-            all = all.filter(c => c !== id)
-        }
-        setChecked(all)
-    }
     useEffect(() => {
-        getAllProducts()
-    }, [])
+        if (page === 1) return;
+        loadMore();
+    }, [page]);
+    //load more
+    const loadMore = async () => {
+        try {
+            setLoading(true);
+            const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+            setLoading(false);
+            setProducts([...products, ...data?.products]);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    };
+
+    // filter by cat
+    const handleFilter = (value, id) => {
+        let all = [...checked];
+        if (value) {
+            all.push(id);
+        } else {
+            all = all.filter((c) => c !== id);
+        }
+        setChecked(all);
+    };
+    useEffect(() => {
+        if (!checked.length || !radio.length) getAllProducts();
+    }, [checked.length, radio.length]);
+
+    useEffect(() => {
+        if (checked.length || radio.length) filterProduct();
+    }, [checked, radio]);
+
+    //get filterd product
+    const filterProduct = async () => {
+        try {
+            const { data } = await axios.post("/api/v1/product/product-filters", {
+                checked,
+                radio,
+            });
+            setProducts(data?.products);
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
-        <Layout title={"all product-best offers"}>
-            <div className='row mt-3'>
-                <div className='col-md-3'>
-                    <h4 className='text-center'>filter by Categories</h4>
-                    <div className='d-flex flex-column'>
-                        {Categories?.map((c) => (
-                            <Checkbox key={c._id} onChange={(e) => handleFilter(e.target.checked, c._id)}>
+        <Layout title={"ALl Products - Best offers "}>
+            <div className="container-fluid row mt-3">
+                <div className="col-md-2">
+                    <h4 className="text-center">Filter By Category</h4>
+                    <div className="d-flex flex-column">
+                        {categories?.map((c) => (
+                            <Checkbox
+                                key={c._id}
+                                onChange={(e) => handleFilter(e.target.checked, c._id)}
+                            >
                                 {c.name}
                             </Checkbox>
                         ))}
                     </div>
+                    {/* price filter */}
+                    <h4 className="text-center mt-4">Filter By Price</h4>
+                    <div className="d-flex flex-column">
+                        <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                            {Prices?.map((p) => (
+                                <div key={p._id}>
+                                    <Radio value={p.array}>{p.name}</Radio>
+                                </div>
+                            ))}
+                        </Radio.Group>
+                    </div>
+                    <div className="d-flex flex-column">
+                        <button
+                            className="btn btn-danger"
+                            onClick={() => window.location.reload()}
+                        >
+                            RESET FILTERS
+                        </button>
+                    </div>
                 </div>
-                <div className='col-md-9'>
-                    {JSON.stringify(checked, null, 4)}
-                    <h1 className='text-center'>All Products</h1>
-                    <div className='d-flex flex-wrap'>
+                <div className="col-md-9">
+                    <h1 className="text-center">All Products</h1>
+                    <div className="d-flex flex-wrap">
                         {products?.map((p) => (
                             <div className="card m-2" style={{ width: "18rem" }}>
                                 <img
@@ -79,17 +145,33 @@ const HomePage = () => {
                                 />
                                 <div className="card-body">
                                     <h5 className="card-title">{p.name}</h5>
-                                    <p className="card-text">{p.description}</p>
+                                    <p className="card-text">
+                                        {p.description.substring(0, 30)}...
+                                    </p>
+                                    <p className="card-text"> $ {p.price}</p>
                                     <button class="btn btn-primary ms-1">More Details</button>
-                                    <button class="btn btn-secondary ms-1">Add to Cart</button>
+                                    <button class="btn btn-secondary ms-1">ADD TO CART</button>
                                 </div>
                             </div>
                         ))}
                     </div>
+                    <div className="m-2 p-3">
+                        {products && products.length < total && (
+                            <button
+                                className="btn btn-warning"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    setPage(page + 1);
+                                }}
+                            >
+                                {loading ? "Loading ..." : "Loadmore"}
+                            </button>
+                        )}
+                    </div>
                 </div>
             </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
